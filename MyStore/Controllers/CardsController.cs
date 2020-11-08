578286c2540505a319311466e60e99b76f1eadc9
Ruslan <ruslan878707@gmail.com>
2020-11-08 14:10:55 +0300

    Add card layout

---
+++ MyStore/Controllers/CardsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyStore.Controllers
+{
+    public class CardsController : Controller
+    {
+        [Route("cards/{id}")]
+        public IActionResult Index(int id)
+        {
+
+
+            return View();
+        }
+    }
+}
